29d950dc558979989c93d83f72628ce5cfbc1f27
JavadJam01 <javadjam01@gmail.com>
2026-09-02 11:59:12 +0330

    refactor: clean up header files for battery and ble service

--- slouchd-tag/firmware/include/ble_service.h
+++ slouchd-tag/firmware/include/ble_service.h
@@ -1,4 +1,5 @@
 #pragma once
+
 #include <Arduino.h>
 #include <NimBLEDevice.h>
 #include "config.h"
@@ -9,12 +10,17 @@ public:
     void begin();
     void sendTelemetry(float pitch, float roll, float baseline, float delta, bool isSlouching, bool isCalibrated);
     bool isConnected() const { return _isConnected; }
-    void setConnected(bool c) { _isConnected = c; }
+
+    void setConnected(bool connected) { _isConnected = connected; }
+
 private:
     NimBLEServer* _pServer;
     NimBLECharacteristic* _pDataChar;
+    NimBLECharacteristic* _pCmdChar;
+    NimBLECharacteristic* _pBatChar;
     bool _isConnected;
     uint8_t _packetSeq;
+    uint8_t _lastNotifiedBatPct;
 };
 
 extern BleServiceManager Ble;
